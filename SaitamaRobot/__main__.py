d3e40d0b93c53cbef3a583ceccb09f4711ed4658
Adrew <64140254+strange552@users.noreply.github.com>
2020-11-05 11:29:33 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -55,21 +55,21 @@ PM_START_TEXT = """
 <b>Hi {}, my name is {}! 
 I am an Anime themed group management bot.
 Build by weebs for weebs, I specialize in managing anime and similar themed groups.
-You can find my list of available commands with /help.<b/>
+You can find my list of available commands with /help.</b>
 """
 
 HELP_STRINGS = """
 <b>Hey there! My name is *{}*.
 I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
-the things I can help you with.<b>
+the things I can help you with.</b>
 
-*Main* commands available:
+<b>*Main* commands available:
  • /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
  • /donate: information on how to donate!
  • /settings:
    • in PM: will send you your settings for all supported modules.
-   • in a group: will redirect you to pm, with all that chat's settings.
+   • in a group: will redirect you to pm, with all that chat's settings.</b>
 
 
 {}
