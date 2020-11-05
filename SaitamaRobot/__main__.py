04a59aeb38146333e13698281962779743738625
Adrew <64140254+strange552@users.noreply.github.com>
2020-11-05 11:57:59 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -59,9 +59,9 @@ You can find my list of available commands with /help.*
 """
 
 HELP_STRINGS = """
-<b>Hey there! My name is *{}*.
+*Hey there! My name is {}.
 I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
-the things I can help you with.</b>
+the things I can help you with.*
 
  *Main commands available:
  • /help: PM's you this message.
